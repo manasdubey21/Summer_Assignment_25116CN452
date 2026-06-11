13d0523ca6557c7f208dd05c4e5e533f8238a1eb
Manas Dubey <manasdubey216@gmail.com>
2026-06-11 20:37:41 +0530

    Add files via upload

---
+++ day9/Q33.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 5;
+    int j;
+
+    while (i >= 1)
+    {
+        j = 1;
+        while (j <= i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i--;
+    }
+
+    return 0;
+}
---
+++ day9/Q36.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    int size = 5;
+
+    for (i = 0; i < size; i++)
+    {
+        for (j = 0; j < size; j++)
+        {
+            if (i == 0 || i == size - 1 || j == 0 || j == size - 1)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
