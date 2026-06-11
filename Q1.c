084e05d8368dcd4cc5ef2b48082954a571b86538
Manas Dubey <manasdubey216@gmail.com>
2026-06-11 12:53:48 +0530

    Day1 uploded.

---
+++ Q1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int n, sum = 0;
+    printf("Enter a positive integer: ");
+    scanf("%d", &n);
+    if (n < 1) {
+        printf("Please enter a valid natural number (greater than 0).\n");
+        return 1;
+    }
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("The sum of the first %d natural numbers is: %d\n", n, sum);
+
+    return 0;
+}
